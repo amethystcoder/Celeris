97c1ee21b2f4da1deefbc1f99af789f0e00516bd
Rotimi Collins <75532154+amethystcoder@users.noreply.github.com>
2026-06-21 12:28:40 +0000

    changed to cpp string concat and memcpy

--- src/net/clientsock.cpp
+++ src/net/clientsock.cpp
@@ -1,4 +1,5 @@
 #include "net/clientsock.h"
+#include <cstring>
 
 Celeris::ClientSocket::~ClientSocket() noexcept
 {
@@ -24,20 +25,33 @@ std::string Celeris::ClientSocket::sendGetRequestWithSocket(CleanSocket* tcpSock
 	}
 
 	sockaddr_in Address{};
-	memcpy_s(&Address, sizeof(Address), results->ai_addr, results->ai_addrlen);
+	auto addr_size = sizeof(Address);
+
+	//test comparison of sizes (will possibly move to a better system for memory size checks )
+
+	if (addr_size >= static_cast<size_t>(results->ai_addrlen))
+	{
+		std::memcpy(&Address,results->ai_addr,results->ai_addrlen);
+	}
+	//memcpy_s(&Address, sizeof(Address), results->ai_addr, results->ai_addrlen);
 
 
 	freeaddrinfo(results);
 
 	connect(tcpSocketIPV4->Get(), reinterpret_cast<const sockaddr*>(&Address), sizeof(Address));
 
-	char request[1024] = "GET / HTTP/1.1\r\n";
-	strcat_s(request, sizeof(request), origin);
-	strcat_s(request, sizeof(request), "Connection: close\r\nUser-Agent:TestAgent\r\n\r\n");
+	//trying string instead
+	//char request[1024] = "GET / HTTP/1.1\r\n";
+	std::string request = "GET / HTTP/1.1\r\n";
+	//strcat_s(request, sizeof(request), origin);
+	request += origin;
+	//strcat_s(request, sizeof(request), "Connection: close\r\nUser-Agent:TestAgent\r\n\r\n");
+	request += "Connection: close\r\nUser-Agent:TestAgent\r\n\r\n";
 
-	send(tcpSocketIPV4->Get(), request, sizeof(request), 0);
+	send(tcpSocketIPV4->Get(), request.c_str(), request.size() + 1, 0);
 
 	char buffer[1024]{};
+	//work on recv here for larger responses than 1024 bytes
 	recv(tcpSocketIPV4->Get(), buffer, sizeof(buffer), 0);
 
 	return buffer;
